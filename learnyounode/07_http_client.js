7d6cf91e771164b57d1dd594f93434dcc0c7310b
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-08-12 22:13:29 -0700

    Add HTTP Client code

---
+++ learnyounode/07_http_client.js
@@ -0,0 +1,15 @@
+var http = require('http')
+
+var url = process.argv[2];
+
+http.get(url, function(res) {
+	res.setEncoding('utf8');
+//	res.on('data', console.log);
+//	res.on('error', console.error);
+	res.on("data", function(data) {
+		console.log(data);
+	});
+	res.on('error', function(error) {
+		console.log(error);
+	});
+});
